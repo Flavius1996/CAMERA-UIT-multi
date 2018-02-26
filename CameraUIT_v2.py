a4d8866d9d6b424a0fc07258d2988702513ea13c
Hoang Huu Tin <fd19919@gmail.com>
2018-02-26 22:39:46 +0700

    Add capturing method 1 (scheduling task) + Remove easydict

--- CameraUIT_v2.py
+++ CameraUIT_v2.py
@@ -7,8 +7,6 @@
 #  Additional Libaries:
 #        opencv-python
 #        schedule - scheduluing tasks - install: pip install schedule
-#        easydict
-#
 #
 # @author: Hoang Huu Tin
 # --------------------------------------------------------
@@ -19,6 +17,7 @@ import os
 import argparse
 import time, datetime
 from threading import Thread
+import shutil
 
 import schedule        # install by: pip install schedule
 
@@ -31,31 +30,33 @@ def checkCamera(link, name):
     vidcap = cv2.VideoCapture(link)
     success, image = vidcap.read()  
     
-    date = time.strftime(CFG.DATE_FORMAT)
-    time_hms = time.strftime(CFG.TIME_FORMAT)
+    date = time.strftime(CFG['DATE_FORMAT'])
+    time_hms = time.strftime(CFG['TIME_FORMAT'])
     name = format_imagefilename(link, name, date, time_hms, 0) 
-    test_img_path = './test_imgs' + name
+    test_img_path = './test_imgs' + '/' + name
 
     cv2.imwrite(test_img_path, image)
 
-    print("Camera info: ")
+    print("\tCamera info: ")
     # Find OpenCV version
     (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
     # Get fps of video
     if int(major_ver)  < 3 :
         fps = int(round(vidcap.get(cv2.cv.CV_CAP_PROP_FPS)))
-        print ("\tCamera\'s fps: {0}".format(fps))
+        print ("\t\tCamera\'s fps: {0}".format(fps))
     else :
         fps = int(round(vidcap.get(cv2.CAP_PROP_FPS)))
-        print ("\tCamera\'s fps: {0}".format(fps))
-    print('\tCamera\'s width = {}'.format(int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))))
-    print('\tCamera\'s height = {}'.format(int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+        print ("\t\tCamera\'s fps: {0}".format(fps))
+    print('\t\tCamera\'s width = {}'.format(int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))))
+    print('\t\tCamera\'s height = {}'.format(int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+    
+    print('\tCaptured test image: {}'.format(test_img_path))
     
     vidcap.release()
 
     return success
 
-def ExtractFrame_FromCameraLink(camera_link, camera_name, start_datetime, end_datetime):
+def ExtractFrame_FromCameraLink(camera_link, camera_name, start_datetime, end_datetime, sched_method):
     '''
         Extract keyframes from camera stream with sampling rate (number of frames will be taken per second)
         Example:
@@ -71,18 +72,18 @@ def ExtractFrame_FromCameraLink(camera_link, camera_name, start_datetime, end_da
         return
 
     # Get date info
-    date = time.strftime(CFG.DATE_FORMAT)
+    date = time.strftime(CFG['DATE_FORMAT'])
     start_time = time.strftime("%H:%M:%S")    # Use for log
     
-    sampling_rate = float(CFG.SAMPLING_RATE)
+    sampling_rate = float(CFG['SAMPLING_RATE'])
 
-    if not os.path.exists(camera_name):
-        os.mkdir(camera_name)
+    if not os.path.exists(CFG['STORE_PATH'] + camera_name):
+        os.mkdir(CFG['STORE_PATH'] + camera_name)
   
     # Create sub-folder for each day in local disk
     folder_day = date
-    if not os.path.exists(camera_name + '/' + folder_day):
-        os.mkdir(camera_name + '/' + folder_day)
+    if not os.path.exists(CFG['STORE_PATH'] + camera_name + '/' + folder_day):
+        os.mkdir(CFG['STORE_PATH'] + camera_name + '/' + folder_day)
     
     ############################# Calculate framestep ############################################## 
     # opencv video capture
@@ -98,21 +99,13 @@ def ExtractFrame_FromCameraLink(camera_link, camera_name, start_datetime, end_da
         fps = int(round(vidcap.get(cv2.CAP_PROP_FPS)))
         
     framestep = max(1, min(int(round(fps / sampling_rate)), int(fps)))
-    
-    
-    print("="*40)
-    print('Extract frames from UIT CAMERA')
-    print("Date: {}".format(time.strftime("%d/%m/%Y")))
-    print("Camera Link: {}".format(camera_link))
-    print("Sampling rate: {} frames/sec".format(sampling_rate))
-    print ("Camera\'s fps: {0}".format(fps)) 
-    print('framestep = {} - extract one frame after each framestep'.format(framestep))
-
         
     # count keyframe
     count_kf = 0
     # count loop
     count = 0
+    
+    end_t = int(end_datetime.strftime("%H%M"))
     
     ############################# CAPTURING LOOP ############################################## 
     # loop through all frame, only get frame with sampling_rate
@@ -122,39 +115,46 @@ def ExtractFrame_FromCameraLink(camera_link, camera_name, start_datetime, end_da
         if (count % framestep == 0):
             count_kf += 1  
             
-            if (count_kf % 100 == 0) or (count_kf == 1):    
-                print('\t[{}] - frame {}: Extracting at time: {}'.format(camera_name, count_kf, time.strftime("%H:%M:%S")))
-            
-            time_hms = time.strftime(CFG.TIME_FORMAT)
-            imgfilepath = camera_name + "/" + folder_day + "/" 
+            time_hms = time.strftime(CFG['TIME_FORMAT'])
+            imgfilepath = CFG['STORE_PATH'] + camera_name + "/" + folder_day + "/" 
             imgfilepath += format_imagefilename(camera_link, camera_name, date, time_hms, count_kf)
-            cv2.imwrite(imgfilepath, image, [int(cv2.IMWRITE_JPEG_QUALITY), CFG.IMAGE_QUALITY])
+            
+            # only print screen after each 100 frames captured
+            if (count_kf % 100 == 0) or (count_kf == 1):    
+                print('[{}] - frame {}: Extracting at time: {}'.format(camera_name, count_kf, time_hms))
+            
+            # save frame to file
+            cv2.imwrite(imgfilepath, image, [int(cv2.IMWRITE_JPEG_QUALITY), CFG['IMAGE_QUALITY']])
                 
         # Check end time
-        cur_t = datetime.datetime.now()
-        if cur_t > end_datetime:
+        if (sched_method == 2):      # CAPTURING METHOD 2
+            cur_dt = datetime.datetime.now()
+            if cur_dt > end_datetime:
+                break 
+        elif (sched_method == 1):    # CAPTURING METHOD 1
+            cur_t = int(datetime.datetime.now().strftime("%H%M"))
+            if cur_t >= end_t:
+                break
+        else:
             break
-        
+            
         count += 1
 
     vidcap.release()
     
-    print("="*40)
-    print('\nDone!\nDay: {}\nTotal frames ={}'.format(date, count_kf))
-    print('Local folder: {}'.format(folder_day))
-
+    print("="*20 + " Done Camera: " + camera_name + " - Date: " + date + ' ' + "="*20)
     # Writing Log
-    log_filename = date + '_' + camera_name
-    log_path = CFG.LOGS_PATH + log_filename
+    log_filename = date + '_' + camera_name + '.txt'
+    log_path = CFG['LOGS_PATH'] + log_filename
+    end_time = time.strftime("%H:%M:%S")    # Use for log
     with open(log_path, "a") as log:
-        log.write("="*40 + '\r\n')
         log.write("Date: {}\r\n".format(date))
         log.write('Camera name: {}\r\n'.format(camera_name))
         log.write('Camera link: {}\r\n'.format(camera_link))
         log.write('Sampling rate: {} frames/s\r\n'.format(sampling_rate))
         log.write('Start time: {}\r\n'.format(start_time))
-        log.write('End time: {}\r\n'.format(CFG.end_time))
-        log.write('Image quality: {}\r\n'.format(CFG.IMAGE_QUALITY))
+        log.write('End time: {}\r\n'.format(end_time))
+        log.write('Image quality: {}\r\n'.format(CFG['IMAGE_QUALITY']))
         log.write('\r\nTotal captured frames = {}\r\n'.format(count_kf))
         
 def parse_args():
@@ -180,14 +180,19 @@ def parse_args():
 def INIT():
     '''Initialize'''
 
-    if not os.path.exists(CFG.STORE_PATH):
-        os.mkdir(CFG.STORE_PATH)
+    if not os.path.exists(CFG['STORE_PATH']):
+        os.mkdir(CFG['STORE_PATH'])
 
-    if not os.path.exists(CFG.LOGS_PATH):
-        os.mkdir(CFG.LOGS_PATH)
+    if not os.path.exists(CFG['LOGS_PATH']):
+        os.mkdir(CFG['LOGS_PATH'])
 
     if not os.path.exists('./test_imgs'):
         os.mkdir('./test_imgs')
+    else:  
+        # Delete test files after each run
+        shutil.rmtree('./test_imgs')
+        os.mkdir('./test_imgs')
+            
 
 def main():
     
@@ -204,51 +209,70 @@ def main():
 
     # Print screen
     print("="*20 + 'Capturing frames from UIT CAMERA' + "="*20)
-    print('Store path: {}'.format(CFG.STORE_PATH))
-    print('Logs path: {}'.format(CFG.LOGS_PATH))
-    print('Sampling rate: {} frames/s'.format(CFG.SAMPLING_RATE))
-    print('Image quality: {}'.format(CFG.IMAGE_QUALITY))
-    print('Filename format: {}'.format(CFG.IMAGE_FILENAME_STRINGFORMAT))
-    print('Date format: {}'.format(CFG.DATE_FORMAT))
-    print('Time format: {}'.format(CFG.TIME_FORMAT))
-    print('Start time: {}'.format(CFG.START_TIME))
-    print('End time: {}'.format(CFG.END_TIME))
-    
-    
-    print('\nChecking capturing cameras...')
-    for camera in CAMERA_LIST:
-        print('Camera: {}'.format(camera['name']))
-        print('Link: {}'.format(camera['link']))
-        if checkCamera(camera['link'], camera['name']):
-            print('Status: OK!')
-            print()
-        else:
-            # Dummy code, above exception in checkCamera() will print error message instead of this
-            print('Something error!')    
-            return
-    print('Check test images in ./test_imgs folder.')
+    print('Store path: {}'.format(CFG['STORE_PATH']))
+    print('Logs path: {}'.format(CFG['LOGS_PATH']))
+    print('Sampling rate: {} frames/s'.format(CFG['SAMPLING_RATE']))
+    print('Image quality: {}'.format(CFG['IMAGE_QUALITY']))
+    print('Filename format: {}'.format(CFG['IMAGE_FILENAME_STRINGFORMAT']))
+    print('Date format: {}'.format(CFG['DATE_FORMAT']))
+    print('Time format: {}'.format(CFG['TIME_FORMAT']))
 
-    if CFG.CAPTURING_TIME != 0:
+    print('Capturing time: {}'.format(CFG['CAPTURING_TIME']))
+    print(CFG)
+    if CFG['CAPTURING_TIME'] > 0:
         # CAPTURING METHOD 2: start rightnow, end after X minutes
-        plus_minute = int(CFG.CAPTURING_TIME)
+        plus_minute = int(CFG['CAPTURING_TIME'])
         start_datetime = datetime.datetime.now()
         end_datetime = start_datetime + datetime.timedelta(minutes = plus_minute)
 
         for camera in CAMERA_LIST:
-            thread1 = Thread(target = ExtractFrame_FromCameraLink, args = (camera['link'], camera['name'], start_datetime, end_datetime))
+            thread1 = Thread(target = ExtractFrame_FromCameraLink, args = (camera['link'], camera['name'], start_datetime, end_datetime, 2))
             thread1.start()
 
     else:
         # CAPTURING METHOD 1: scheduling task
-        print('\nCurrent system time is: {}'.format(time.strftime("%H:%M:%S")))
+        print('Start date: {}'.format(CFG['START_DATE']))
+        print('End date: {}'.format(CFG['END_DATE']))
+        print('Start time: {}'.format(CFG['START_TIME']))
+        print('End time: {}'.format(CFG['END_TIME']))
+    
+        # Check time logic
+        start_datetime = datetime.datetime.strptime(CFG['START_DATE'] + ' ' + CFG['START_TIME'], '%d/%m/%Y %H:%M')
+        end_datetime = datetime.datetime.strptime(CFG['END_DATE'] + ' ' + CFG['END_TIME'], '%d/%m/%Y %H:%M')
+        now = datetime.datetime.now()
+        if (start_datetime < now):
+            raise ValueError('ERROR: Start datetime ({}) < now ({})'.format(start_datetime, now))
+        if (start_datetime >= end_datetime):
+            raise ValueError('ERROR: Start datetime ({}) > End datetime ({})'.format(start_datetime, end_datetime))
+        
+        # Check camera
+        print('\nChecking capturing cameras...')
+        for camera in CAMERA_LIST:
+            print('Camera: {}'.format(camera['name']))
+            print('Link: {}'.format(camera['link']))
+            if checkCamera(camera['link'], camera['name']):
+                print('\tStatus: OK!')
+                print('')
+            else:
+                # Dummy code, those exceptions in checkCamera() will raise error message instead of this
+                print('Something error!')    
+                return
+        
+        # Waiting to start
+        print('\nCurrent system datetime is: {}'.format(time.strftime("%d/%m/%Y %H:%M:%S")))
         print('Waiting to start time ...')
         
-        schedule.every().day.at(CFG.start_time).do(ExtractFrame_FromCameraLink, args.camera_link, 
-                              args.camera_name)
+        for camera in CAMERA_LIST:
+            schedule.every().day.at(CFG['START_TIME']).do(ExtractFrame_FromCameraLink, camera['link'], camera['name'], start_datetime, end_datetime, 1)
         
         while True:
             schedule.run_pending()
             time.sleep(1) # wait 1s
+            
+    print('#'*40)
+    print('#'*40)
+    print('!!! END SCRIPT !!!')
+    
     
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -4,52 +4,50 @@ Configuration for camera UIT:
     + load config from yml file
 '''
 
-from easydict import EasyDict as edict
-
 
 ############################# DEFAULT OPTIONS #########################
-__C = edict()
+__C = dict()
 # Consumers can get config by:
 #   from config import CFG
 CFG = __C
 
 # Where to store captured frames?
-__C.STORE_PATH = './CAMERA_UIT'
+__C['STORE_PATH'] = './CAMERA_UIT'
 
 # Where to store logs?
-__C.LOGS_PATH = './logs'
+__C['LOGS_PATH'] = './logs'
 
 # Number of frames per second
-__C.SAMPLING_RATE = 5
+__C['SAMPLING_RATE'] = 5.0
 
 # Captured jpeg images quality - max=100% (non compression)
-__C.IMAGE_QUALITY = 100
+__C['IMAGE_QUALITY'] = 100
 
 # Image Filename format
-__C.IMAGE_FILENAME_STRINGFORMAT = '"{}_{}_{}__{}".format(camera_name, date, time, count)'
+__C['IMAGE_FILENAME_STRINGFORMAT'] = '"{}_{}_{}__{}".format(camera_name, date, time, count)'
 # Date, time format in filename
-__C.DATE_FORMAT = "%d%m%Y"
-__C.TIME_FORMAT = "%H-%M-%S"
+__C['DATE_FORMAT'] = "%d%m%Y"
+__C['TIME_FORMAT'] = "%H-%M-%S"
 
 
 ##### CAPTURING METHOD 1: Scheduling capturing
 # Start capturing time of day
   # FORMAT: HH:MM  (24H format)
-__C.START_TIME = '07:00'
+__C['START_TIME'] = '07:00'
 
 # End capturing time
-__C.END_TIME = '08:00'
+__C['END_TIME'] = '08:00'
 
 # Start capturing date
   # FORMAT: dd/MM/YYYY
-__C.START_DATE = '25/02/2018'
+__C['START_DATE'] = '25/02/2018'
 
 # End capturing date
-__C.END_DATE = '28/02/2018'
+__C['END_DATE'] = '28/02/2018'
 
 ##### CAPTURING METHOD 2: Start capturing immediately and end after CAPTURING_TIME minutes
-  # Set 0 for disable method 2, otherwise method 1 will be disable
-__C.CAPTURING_TIME = 0
+  # Set 0 for disable method 2 and using method 1 instead
+__C['CAPTURING_TIME'] = 0
 
 
 
@@ -58,7 +56,7 @@ def _merge_a_into_b(a, b):
     """Merge config dictionary a into config dictionary b, clobbering the
     options in b whenever they are also specified in a.
     """
-    if type(a) is not edict:
+    if type(a) is not dict:
         return
 
     for k, v in a.items():
@@ -74,7 +72,7 @@ def _merge_a_into_b(a, b):
                                                         type(v), k))
 
         # recursively merge dicts
-        if type(v) is edict:
+        if type(v) is dict:
             try:
                 _merge_a_into_b(a[k], b[k])
             except:
@@ -85,18 +83,19 @@ def _merge_a_into_b(a, b):
             
 def load_cfg_from_file(filepath):
     """Load a config yml file and merge it into the default options."""
+    global CFG
     import yaml
     with open(filepath, 'r') as f:
-        yaml_cfg = edict(yaml.load(f))
+        yaml_cfg = dict(yaml.load(f))
 
     _merge_a_into_b(yaml_cfg, __C)
 
     # Normalize
-    if CFG.STORE_PATH[-1] != '/' or CFG.STORE_PATH[-1] != '\\':
-        CFG.STORE_PATH += '/'
+    if CFG['STORE_PATH'][-1] != '/' or CFG['STORE_PATH'][-1] != '\\':
+        CFG['STORE_PATH'] += '/'
         
-    if CFG.LOGS_PATH[-1] != '/' or CFG.LOGS_PATH[-1] != '\\':
-        CFG.LOGS_PATH += '/'
+    if CFG['LOGS_PATH'][-1] != '/' or CFG['LOGS_PATH'][-1] != '\\':
+        CFG['LOGS_PATH'] += '/'
     
 
 
@@ -104,18 +103,17 @@ def format_imagefilename(camera_link, camera_name, date, time, count):
 
     camera_ip = camera_link.split('@')[-1].split(':')[0]
 
-    stringformat = CFG.IMAGE_FILENAME_STRINGFORMAT
+    stringformat = CFG['IMAGE_FILENAME_STRINGFORMAT']
 
     filename = eval(stringformat) + '.jpg'
 
     return filename
 
 
-
 #### DEBUG ####
-print(CFG)
-print()
-file = './cfgs/test.yml'
-load_cfg_from_file(file)
-print(CFG)
-print(format_imagefilename('rtsp://test:12345@192.168.75.27:554', 'test','26022018', '12:04', 5))
+#print(CFG)
+#print()
+#file = './cfgs/test.yml'
+#load_cfg_from_file(file)
+#print(CFG)
+#print(format_imagefilename('rtsp://test:12345@192.168.75.27:554', 'test','26022018', '12:04', 5))
